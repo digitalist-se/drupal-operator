9974d952d756097a0cad713ec3f45a000457a20e
Mikke Schirén <mikke.schiren@digitalistgroup.com>
2019-11-09 10:28:24 +0100

    move more settings to variables

--- rebuild.sh
+++ rebuild.sh
@@ -7,6 +7,5 @@ docker push ozziio/drupal-operator:v0.0.2
 # redeploy operator
 microk8s.kubectl delete -f deploy/
 microk8s.kubectl delete -f deploy/crds/ozzio_v1alpha2_drupal_crd.yaml
-#microk8s.kubectl apply -f deploy/
 microk8s.kubectl apply -f deploy/crds/ozzio_v1alpha2_drupal_crd.yaml
 microk8s.kubectl apply -f deploy/
